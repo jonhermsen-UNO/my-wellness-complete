9096c3d4633e9c81f3dab57b78c7b883f332f23b
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-03-07 05:11:41 -0600

    feat: add stub for all wellness dimensions

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const path = require('path');
 const controller = {};
 
@@ -6,6 +7,18 @@ controller.getHome = (request, response) => {
   const fields = {
     pageTitle: 'My Wellness Complete',
     pageDescription: 'Welcome to My Wellness Complete, a total wellness tracker that integrates with Google Fit!',
+    profile: getProfile(request),
+  };
+
+  response.render(view, fields);
+};
+
+// a single stub is used for all modules that still need implemented
+controller.getStub = (request, response) => {
+  const view = getView('/stub');
+  const fields = {
+    pageTitle: `Wellness Dimension: ${_.startCase(request.path)}`,
+    pageDescription: 'This dimension of My Wellness Complete is not yet implemented.',
     profile: getProfile(request),
   };
 
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const controller = require('../controllers');
 const router = express.Router();
 
-router
-  .route('/')
-  .get(controller.getHome);
+router.route('/').get(controller.getHome);
+router.route('/environmental').get(controller.getStub);
+router.route('/financial').get(controller.getStub);
+router.route('/spiritual').get(controller.getStub);
+router.route('/vocational').get(controller.getStub);
+router.route('/emotional').get(controller.getStub);
+router.route('/social').get(controller.getStub);
+router.route('/physical').get(controller.getStub);
+router.route('/mental').get(controller.getStub);
 
 module.exports = router;
