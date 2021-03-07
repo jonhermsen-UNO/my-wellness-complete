caf8671c6bcb1e9b464ed94031c75ce150bc1d18
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-03-06 18:53:48 -0600

    feat: add authentication

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -6,6 +6,7 @@ controller.getHome = (request, response) => {
   const fields = {
     pageTitle: 'My Complete Wellness',
     pageDescription: 'Welcome to My Complete Wellness, a total wellness tracker that integrates with Google Fit!',
+    profile: getProfile(request),
   };
 
   response.render(view, fields);
@@ -16,4 +17,22 @@ module.exports = controller;
 function getView(page) {
   const view = path.join(__dirname, '../views', page);
   return view;
+}
+
+function getProfile(request) {
+  const profile = {
+    name: '',
+    picture: '',
+    hasLogin: false,
+  };
+
+  if (request && request.session && request.session.passport && request.session.passport.user) {
+    const user = request.session.passport.user;
+
+    profile.hasLogin = true;
+    profile.name = user.displayName;
+    profile.picture = user.photos ? user.photos[0].value : '';
+  }
+
+  return profile;
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,37 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const keys = require('./private/keys');
+const session = require('express-session');
+app.use(
+  session({
+    secret: keys.sessionSecret,
+    saveUninitialized: false,
+    resave: false,
+    rolling: true,
+  })
+);
+
+/** Authentication **/
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleID,
+      clientSecret: keys.googleSecret,
+      callbackURL: '/login/redirect',
+    },
+    (accessToken, refreshToken, profile, callback) => callback(null, profile)
+  )
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, callback) => callback(null, user));
+passport.deserializeUser((user, callback) => callback(null, user || false));
+
 /** Layout Engine **/
 
 app.use(require('express-ejs-layouts'));
@@ -20,9 +51,16 @@ app.set('layout', path.join(__dirname, './app/layouts/main'));
 const routes = {};
 routes.app = require('./app/routes');
 routes.api = require('./api/routes');
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/', routes.app);
+app.use('/app/', express.static(path.join(__dirname, 'public')));
+app.use('/app/', routes.app);
 app.use('/api/', routes.api);
+app.get('/login', passport.authenticate('google', { scope: ['profile'] }));
+app.get('/login/redirect', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/' }));
+app.get('/logout', (request, response) => {
+  request.logout();
+  response.redirect('/');
+});
+app.use('/', (_, response) => response.redirect('/app/'));
 
 /** Server **/
 
